0883d268d5d07ec1825c43510ebd5e2647afef8e
Leonardo Barcelos <leobarcelos12@gmail.com>
2019-06-21 12:34:19 -0300

    Fitting para polinômios de grau N finalizado

--- aula17/regressao_linear.c
+++ aula17/regressao_linear.c
@@ -15,19 +15,55 @@ double** getPoints(char* fileName, int numberOfPoints) {
 }
 
 int main() {
-	Matrix *i;
-	// m->data = {{1.,4.,-7.},{-1.,-3.,10.},{-2.,-6.,12.}};
-	double** points = getPoints("pontos.dat", 100);
+    char fileName[100];
+    int numberOfPoints = 0, degree = 0;
+    printf("Digite o nome do arquivo onde estão os pontos:\n");
+    scanf("%s", fileName);
+    printf("Digite o número de pontos:\n");
+    scanf("%d", &numberOfPoints);
+    printf("Digite o grau do polinômio:\n");
+    scanf("%d", &degree);
 
-	// Matrix* m = createMatrix(2, 2);
-	// m->data = points;
+    double** points = getPoints(fileName, numberOfPoints);
 
-	// printMatrix(m);
-	// i = getInverse(m);
-	// printMatrix(i);
+    BM* b = startBenchmark("Tempo de Execução da Regressão Linear");
+    Matrix* incognito = getIncognitoMatrix(points, degree, numberOfPoints);
+    stopBenchmark(b);
+    printf("Os coeficientes são:\n");
+    for(int i = 0; i < incognito->lines; i++) {
+        printf("α%d = %lf\n", i, incognito->data[i][0]);
+    }
 
-	i = getIncognitoMatrix(points, 2, 100);
-	printMatrix(i);
+    GnuPipe g = initializeGnuPipe();
 
-	free(i);
+	fprintf(g, "set grid\n");
+	fprintf(g, "set title 'Regressão Linear'\n");
+	fprintf(g, "set zeroaxis\n");
+	fprintf(g, "f(x) = ");
+	for(int i = incognito->lines - 1; i >= 0; i--) {
+		if(i == incognito->lines - 1) {
+			fprintf(g, "%lf*x**%d", incognito->data[i][0], i);
+		} else {
+			char s = incognito->data[i][0] >= 0 ? '+' : '-';
+			fprintf(g, " %c %lf*x**%d", s, incognito->data[i][0], i);
+		}
+	}
+	fprintf(g, "\n");
+
+	fprintf(g, "plot '-' title 'Pontos', f(x) title '");
+
+	fprintf(g, "f(x) = ");
+	for(int i = incognito->lines - 1; i >= 0; i--) {
+		if(i == incognito->lines - 1) {
+			fprintf(g, "%lfx^%d", incognito->data[i][0], i);
+		} else {
+			char s = incognito->data[i][0] >= 0 ? '+' : '-';
+			fprintf(g, " %c %lfx^%d", s, incognito->data[i][0], i);
+		}
+	}
+	fprintf(g, "'\n");
+    for (int i = 0; i < numberOfPoints; i++) {
+        fprintf(g, "%lf %lf\n", points[i][0], points[i][1]);
+    }
+    fprintf(g, "e\n");
 }
--- geraPontosLineares.py
+++ geraPontosLineares.py
@@ -1,21 +1,29 @@
 import sys
 import random
 
-def f(r1, r2, x):
-	return r1*x + r2 + random.randint(-100, 100)
+def f(coefficients, x):
+	result = 0
+	for degree, coefficient in enumerate(coefficients):
+		result += coefficient * x ** degree
+	return result + random.randint(-100, 100)
 
 def main():
 	caminhoArquivo = sys.argv[1]
 	pN = int(sys.argv[2])
+	degree = int(sys.argv[3]) + 1
 	file = open("{}/pontos.dat".format(caminhoArquivo), "w")
-	r1 = random.randint(-10, 10)
-	r2 = random.randint(-10, 10)
+	coefficients = []
+	for i in range(degree):
+		coefficients.append(random.randint(-10, 10))
 
-	print("f(x) = {}x^2 + {}".format(r1, r2))
+	func = ""
+	for degree, coefficient in enumerate(coefficients):
+		func += f"+ {coefficient}x^{degree}"
 
+	print(f"f(x) = {func}")
 
-	for i in range(0, pN):
-		file.write("{} {}\n".format(i, f(r1, r2, i)))
+	for i in range(-pN, pN):
+		file.write("{} {}\n".format(i, f(coefficients, i)))
 
 	file.close()
 
